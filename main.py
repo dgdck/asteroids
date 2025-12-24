21cbf9105327af59edb79bfea7d3549b49b9dc06
dgdck <d.godecke@gmail.com>
2025-12-24 17:09:02 +0100

    fix asteroids spawning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from asteroid import Asteroid
-from asteroidfield import Asteroidfield
+from asteroidfield import AsteroidField
 from player import Player
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
@@ -12,7 +12,7 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    Clock = pygame.time.Clock()
+    clock = pygame.time.Clock()
     dt = 0
 
     # Groups
@@ -24,12 +24,12 @@ def main():
     # Add to groups
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
-    Asteroidfield.containers = (updatable)
+    AsteroidField.containers = updatable
 
 
     # Make Player object
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    asteroidfield = Asteroidfield()
+    asteroid_field = AsteroidField()
 
     # Game loop
     while True:
@@ -41,19 +41,19 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        # Update screen
-        screen.fill("black")
-        dt = Clock.tick(60) / 1000
-
         # Update objects
         updatable.update(dt)
-        
+
+        # Update screen
+        screen.fill("black")
+
         # Draw objects
-        for object in drawable:
-            object.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         
         # Display
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
