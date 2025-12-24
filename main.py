e2584e14d8c6b1dff37a3ff917f2d452c29a15d6
dgdck <d.godecke@gmail.com>
2025-12-24 13:39:05 +0000

    add clock

--- main.py
+++ main.py
@@ -6,15 +6,20 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        pygame.display.flip()        
+        pygame.display.flip()
+        dt = Clock.tick(60) / 1000
+        
 
 
 if __name__ == "__main__":
